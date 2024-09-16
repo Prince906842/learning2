5603b8cb6d4fcf4c780b1f20ae0a668485c61a19
prince raj <jarvisraj04@gmail.com>
2024-09-16 09:14:57 +0530

    "add nothing"

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 console.log("hi");
-<<<<<<< HEAD
 console.log("hello world");
 
-=======
-console.log("hello");
-console.log("hi,guys my name is prince");
->>>>>>> 742cc962fa1c6a67db410cdef37c5f3256463f1c
